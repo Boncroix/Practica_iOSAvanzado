933399701c20b410da1401c02b2964c2adde2919
Boncroix <boncroix@gmail.com>
2024-02-23 19:34:36 +0100

    Crear estructura de organización para la aplicación

---
+++ DragonBallAPP/DragonBallAPP/Presentation/Views/Splash/SplashVC.swift
@@ -0,0 +1,15 @@
+//
+//  SplashViewController.swift
+//  DragonBallAPP
+//
+//  Created by Jose Bueno Cruz on 23/2/24.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
