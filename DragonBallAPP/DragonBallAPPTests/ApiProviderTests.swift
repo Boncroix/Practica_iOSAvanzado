f4b7ce77531097852f7ed05ebe1f558c8cf9bd3b
Boncroix <boncroix@gmail.com>
2024-03-03 23:47:20 +0100

    hacer test store data provider

--- DragonBallAPP/DragonBallAPPTests/ApiProviderTests.swift
+++ DragonBallAPP/DragonBallAPPTests/ApiProviderTests.swift
@@ -15,6 +15,7 @@ final class ApiProviderTests: XCTestCase {
     private var expectedToken: String = ""
     private var idHero: String = ""
 
+    //MARK: SetUp
     override func setUpWithError() throws {
         try super.setUpWithError()
         let configuration = URLSessionConfiguration.ephemeral
@@ -34,6 +35,7 @@ final class ApiProviderTests: XCTestCase {
         idHero = ""
     }
     
+    //MARK: - TestLogin
     func test_logining() {
         // Given
         let (user, password) = ("user", "password")
@@ -63,9 +65,10 @@ final class ApiProviderTests: XCTestCase {
                 XCTFail("expected fail")
             }
         }
-        wait(for: [expectation], timeout: 0.1)
+        wait(for: [expectation], timeout: 0.2)
     }
     
+    //MARK: - TestLoginFail
     func test_loginFail() {
         // Given
         let (user, password) = ("user", "password")
@@ -95,9 +98,10 @@ final class ApiProviderTests: XCTestCase {
                 XCTAssertEqual(error.description, "Error code 401")
             }
         }
-        wait(for: [expectation], timeout: 0.1)
+        wait(for: [expectation], timeout: 0.2)
     }
     
+    //MARK: - TestGetHeroes
     func test_getHeroes() {
         // Given
         let expectedHero = Hero.init(id: "14BB8E98-6586-4EA7-B4D7-35D6A63F5AA3",
@@ -131,9 +135,41 @@ final class ApiProviderTests: XCTestCase {
                 XCTFail("expected fail")
             }
         }
-        wait(for: [expectation], timeout: 0.4)
+        wait(for: [expectation], timeout: 0.2)
     }
     
+    func test_getHeroesFail() {
+        // Given
+        let expectedHero = Hero.init(id: "14BB8E98-6586-4EA7-B4D7-35D6A63F5AA3",
+                                     name: "Maestro Roshi",
+                                     photo: "https://cdn.alfabetajuega.com/alfabetajuega/2020/06/Roshi.jpg?width=300",
+                                     description: "Es un maestro de artes marciales que tiene una escuela, donde entrenará a Goku y Krilin para los Torneos de Artes Marciales.")
+        
+        MockURLProtocol.handler = { request in
+            XCTAssertEqual(request.httpMethod, "POST")
+            let url = try XCTUnwrap(request.url)
+            XCTAssertEqual(url, self.host.appendingPathComponent("/api/heros/all"))
+            let mockUrl = try XCTUnwrap(Bundle(for: type(of: self)).url(forResource: "heroes", withExtension: "json"))
+            let data = try Data(contentsOf: mockUrl)
+            let response = try  XCTUnwrap(MockURLProtocol.urlResponseFor(url: url, statusCode: 401))
+            
+            return (response, data)
+        }
+        //When
+        let expectation = expectation(description: "Load Heroes")
+        sut.getHeroesWith { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let token):
+                XCTFail("expected fail")
+            case .failure(let error):
+                XCTAssertEqual(error.description, "Error code 401")
+            }
+        }
+        wait(for: [expectation], timeout: 0.2)
+    }
+    
+    //MARK: - TestGetTransformations
     func test_getTransformations() {
         // Given
         let expectedTransformation = Transformation.init(photo: "https://areajugones.sport.es/wp-content/uploads/2020/03/Gorilin.jpg.webp",
@@ -154,7 +190,7 @@ final class ApiProviderTests: XCTestCase {
         }
         //When
         let expectation = expectation(description: "Load Transformations")
-        sut.getTransformationsForHeroWith(id: "D88BE50B-913D-4EA8-AC42-04D3AF1434E3") { result in
+        sut.getTransformationsForHeroWith(id: idHero) { result in
             expectation.fulfill()
             switch result {
             case .success(let transformations):
@@ -168,9 +204,42 @@ final class ApiProviderTests: XCTestCase {
                 XCTFail("expected fail")
             }
         }
-        wait(for: [expectation], timeout: 0.4)
+        wait(for: [expectation], timeout: 0.2)
     }
     
+    func test_getTransformationsFail() {
+        // Given
+        let expectedTransformation = Transformation.init(photo: "https://areajugones.sport.es/wp-content/uploads/2020/03/Gorilin.jpg.webp",
+                                                         hero: nil,
+                                                         id: "616ADA79-EF94-41BE-B4ED-3A44A3F3E2B7",
+                                                         name: "4. Gorilin",
+                                                         description: "Es la fusión de  Krillin y Kid Goku que apareció por primera vez en Dragon Ball Fusions.")
+        
+        MockURLProtocol.handler = { request in
+            XCTAssertEqual(request.httpMethod, "POST")
+            let url = try XCTUnwrap(request.url)
+            XCTAssertEqual(url, self.host.appendingPathComponent("/api/heros/tranformations"))
+            let mockUrl = try XCTUnwrap(Bundle(for: type(of: self)).url(forResource: "transformations", withExtension: "json"))
+            let data = try Data(contentsOf: mockUrl)
+            let response = try  XCTUnwrap(MockURLProtocol.urlResponseFor(url: url, statusCode: 401))
+            
+            return (response, data)
+        }
+        //When
+        let expectation = expectation(description: "Load Transformations")
+        sut.getTransformationsForHeroWith(id: idHero) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let token):
+                XCTFail("expected fail")
+            case .failure(let error):
+                XCTAssertEqual(error.description, "Error code 401")
+            }
+        }
+        wait(for: [expectation], timeout: 0.2)
+    }
+    
+    //MARK: - TestGetLocations
     func test_getLocations() {
         // Given
         let expectedLocation = Location.init(id: "B93A51C8-C92C-44AE-B1D1-9AFE9BA0BCCC",
@@ -191,7 +260,7 @@ final class ApiProviderTests: XCTestCase {
         }
         //When
         let expectation = expectation(description: "Load Locations")
-        sut.getLocationsForHeroWith(id: "D88BE50B-913D-4EA8-AC42-04D3AF1434E3") { result in
+        sut.getLocationsForHeroWith(id: idHero) { result in
             expectation.fulfill()
             switch result {
             case .success(let locations):
@@ -205,61 +274,38 @@ final class ApiProviderTests: XCTestCase {
                 XCTFail("expected fail")
             }
         }
-        wait(for: [expectation], timeout: 0.4)
-    }
-    /*
-    private func setToken() {
-        MockSecureStorage().set(token: self.expectedToken)
-    }
-    
-    func test_getBootcampsWithError() {
-        //Given
-        MockURLProtocol.error = NSError(domain: "io.keepcoding.LoadBootcamps", code: -1008)
-        
-        //When
-        let expectation = expectation(description: "Load Bootcamps Server Error")
-        sut.getBootcamps { result in
-            switch result {
-            case .success(_):
-                XCTFail("Waiting for error")
-            case .failure(let error):
-                XCTAssertNotNil(error)
-            }
-            expectation.fulfill()
-        }
-        
-        //Then
         wait(for: [expectation], timeout: 0.2)
     }
     
-    func test_getBootcampsErrorStatusCode() {
-        //Given
+    func test_getLocationsFail() {
+        // Given
+        let expectedLocation = Location.init(id: "B93A51C8-C92C-44AE-B1D1-9AFE9BA0BCCC",
+                                             latitude: "35.71867899343361",
+                                             longitude: "139.8202084625344",
+                                             date: "2022-02-20T00:00:00Z",
+                                             hero: nil)
+        
         MockURLProtocol.handler = { request in
-            XCTAssertEqual(request.httpMethod, "GET")
+            XCTAssertEqual(request.httpMethod, "POST")
             let url = try XCTUnwrap(request.url)
-            XCTAssertEqual(url, self.host.appendingPathComponent("api/data/bootcamps"))
-      
+            XCTAssertEqual(url, self.host.appendingPathComponent("/api/heros/locations"))
+            let mockUrl = try XCTUnwrap(Bundle(for: type(of: self)).url(forResource: "locations", withExtension: "json"))
+            let data = try Data(contentsOf: mockUrl)
             let response = try  XCTUnwrap(MockURLProtocol.urlResponseFor(url: url, statusCode: 401))
-
-            return (response, Data())
+            
+            return (response, data)
         }
-        
-        
         //When
-        let expectation = expectation(description: "Load Bootcamps Error with Status code")
-        sut.getBootcamps { result in
-            switch result {
-            case .success(_):
-                XCTFail("Waiting for error")
-            case .failure(let error):
-                XCTAssertNotNil(error)
-                XCTAssertEqual(error.description, "Received error status code 401")
-            }
+        let expectation = expectation(description: "Load Locations")
+        sut.getLocationsForHeroWith(id: idHero) { result in
             expectation.fulfill()
+            switch result {
+            case .success(let token):
+                XCTFail("expected fail")
+            case .failure(let error):
+                XCTAssertEqual(error.description, "Error code 401")
+            }
         }
-        
-        //Then
         wait(for: [expectation], timeout: 0.2)
     }
-     */
 }
